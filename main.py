f3e4548e8570f0ac84c8d6a2ce618fe1ccd21f8a
Henry Huy Vu <50112632+henryhuyvu@users.noreply.github.com>
2023-03-26 23:47:55 -0400

    Add descriptive text to clarify on what the output values represent

--- main.py
+++ main.py
@@ -7,20 +7,23 @@ indicator_url = "https://ghoapi.azureedge.net/api/Indicator"
 indicator_response = requests.get(indicator_url)
 
 indicator_json = indicator_response.json()
-print(len(indicator_json))
-print(type(indicator_json))
+print("The dimensions of the raw data list is:",len(indicator_json))
+print("The raw data's type is:",type(indicator_json))
 
-print('\n')
+print('\nThe raw data elements are identified by:')
+count = 1
 for value in indicator_json:
-    print(value)
+    print(count, value)
+    count += 1
+
+print("\nThe number of available indicators is:",len(indicator_json['value']))
+
+print("\nA single indicators raw data is shown below")
 # print(indicator_json['@odata.context'])
 # print(type(indicator_json['value']))
 print(indicator_json['value'][0])
-print(type(indicator_json['value'][0]))
-print(len(indicator_json['value'][0]))
+print("The data type here is:",type(indicator_json['value'][0]))
 
-print('\n')
+print('\nThis raw data can be pulled out into key and value elements as:')
 for keys in indicator_json['value'][0]:
     print(keys,": ", indicator_json['value'][0][keys])
-# print(type(indicator_json['value'][0]))
-# print(indicator_json)
