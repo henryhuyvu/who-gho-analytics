4470c0ea740c13bced4395effa59842a38f02120
Henry Huy Vu <50112632+henryhuyvu@users.noreply.github.com>
2023-06-20 16:48:08 -0400

    Plot rates for region, all countries, highest and lowest rates, and refactor data extract function

--- data_suicide.py
+++ data_suicide.py
@@ -34,81 +34,125 @@ dataFrame_Suicide = pd.DataFrame(data_suicide)
 
 # View data
 columns = list(dataFrame_Suicide.columns)
-print(columns)
 
 # Dropping unnecessary columns and updating column names
 cleanDataFrame_Suicide = dataFrame_Suicide.drop(columns=["Id","IndicatorCode","TimeDimType","Dim1Type","Dim2Type","Dim2","Dim3Type","Dim3","DataSourceDimType","DataSourceDim","Comments","TimeDimensionBegin","TimeDimensionEnd","Date","TimeDimensionValue"])
 cleanDataFrame_Suicide = cleanDataFrame_Suicide.rename(columns={"Dim1":"Sex", "TimeDim":"Year"})
-print(cleanDataFrame_Suicide)
 
 #%% Prepping data for plotting
 spatialWorldBank = list(cleanDataFrame_Suicide.loc[cleanDataFrame_Suicide['SpatialDimType'] == 'WORLDBANKINCOMEGROUP']['SpatialDim'].unique())
 spatialRegions = list(cleanDataFrame_Suicide.loc[cleanDataFrame_Suicide['SpatialDimType'] == 'REGION']['SpatialDim'].unique())
 spatialCountries = list(cleanDataFrame_Suicide.loc[cleanDataFrame_Suicide['SpatialDimType'] == 'COUNTRY']['SpatialDim'].unique())
 
-def extractCountryData(countryCode,sex):    
-    countryData.append(cleanDataFrame_Suicide.loc[
+def extractDataToArray(countryCode,sex,arrayName):    
+    arrayName.append(cleanDataFrame_Suicide.loc[
         (cleanDataFrame_Suicide['SpatialDim'] == countryCode) & 
         (cleanDataFrame_Suicide['Sex'] == sex)
         ].sort_values(by=["Year"]))
     
-def extractRegionData(countryCode,sex):    
-    regionData.append(cleanDataFrame_Suicide.loc[
-        (cleanDataFrame_Suicide['SpatialDim'] == countryCode) & 
-        (cleanDataFrame_Suicide['Sex'] == sex)
-        ].sort_values(by=["Year"]))
-    
+#%% Regional Data 
+regionData = []
+for i in range(len(spatialRegions)):
+    extractDataToArray(spatialRegions[i],"BTSX",regionData)
+
+# Plot all the data
+for i in range(len(spatialRegions)):
+    if i == 0:
+        ax = regionData[i].plot(y="NumericValue",x="Year")
+    else:
+        regionData[i].plot(ax=ax,y="NumericValue",x="Year")
+
+plt.title("Age-standardized suicide rates by region")
+plt.xlabel("Year")
+plt.ylabel("Suicide rate per 100,000 population")
+plt.xlim([2000,2019])
+plt.legend(spatialRegions, bbox_to_anchor=(1, 1.), loc='best', prop={'size': 8}, ncol=4)
+plt.grid()
+plt.show(block=False)
 
 #%% Country Data
 countryData = []
 for i in range(len(spatialCountries)):
-    extractCountryData(spatialCountries[i],"BTSX")
+    extractDataToArray(spatialCountries[i],"BTSX",countryData)
 
 # Plot all the data
 for i in range(len(spatialCountries)):
     if i == 0:
-        ax = countryData[i].plot(y="NumericValue",x="Year")
+        ax = countryData[i].plot(y="NumericValue",x="Year",legend=None, linewidth=0.8)
     else:
-        countryData[i].plot(ax=ax,y="NumericValue",x="Year")
+        countryData[i].plot(ax=ax,y="NumericValue",x="Year",legend=None, linewidth=0.8)
 
-# plt.title("Age-standardized suicide rates by Country")
-# plt.xlabel("Year")
-# plt.ylabel("Suicide rate per 100,000 population")
-# plt.xlim([2000,2019])
-# plt.legend(spatialCountries, bbox_to_anchor=(1, 1.), loc='best', prop={'size': 4}, ncol=4)
-# plt.show(block=False)
+plt.title(f"Age-standardized suicide rates for all {len(spatialCountries)} countries")
+plt.xlabel("Year")
+plt.ylabel("Suicide rate per 100,000 population")
+plt.xlim([2000,2019])
+# plt.legend(spatialCountries, loc='best', bbox_to_anchor=(1,1), prop={'size': 4}, ncol=4)
+plt.locator_params(axis="x", integer=True, tight=True)
+plt.tight_layout()
+plt.subplots_adjust(top=0.88)
+plt.grid()
+plt.show(block=False)
 
-# Looking at the highest rates amongst all of the countries
-print(round(countryData[1].iloc[-1,-3], 1))
-
-print(countryData[1])
-print(spatialCountries)
+#%% Highest rates for N countries amongst all countries
 valueColumnIndex = -3
 latestDataRowIndex = -1
-recentSuicideRates = []
+recentSuicideRates = {}
 for i in range(len(countryData)):
-    recentSuicideRates.append({f"{spatialCountries[i]}":f"{round(countryData[i].iloc[latestDataRowIndex,valueColumnIndex], 1)}"})
-print(type(recentSuicideRates))
-print(len(recentSuicideRates))
-print(recentSuicideRates[0])
-print(max(recentSuicideRates,key=recentSuicideRates.get))
-# print(spatialCountries[i],": ",round(countryData[i].iloc[latestDataRowIndex,valueColumnIndex], 1))
+    recentSuicideRates.update({f"{spatialCountries[i]}":round(countryData[i].iloc[latestDataRowIndex,valueColumnIndex], 3)})
 
-#%% Regional Data 
-# regionData = []
-# for i in range(len(spatialRegions)):
-#     extractRegionData(spatialRegions[i],"BTSX")
+sortedRecentRates = sorted(list(recentSuicideRates.values()))
+numberOfTopCountries = 10
+topRates = []
+topCountries = []
+for n in range(numberOfTopCountries):
+    topRates.append(sortedRecentRates[-(n+1)])
+    topCountries.append(list(recentSuicideRates.keys())[list(recentSuicideRates.values()).index(sortedRecentRates[-(n+1)])])
 
-# # Plot all the data
-# for i in range(len(spatialRegions)):
-#     if i == 0:
-#         ax = regionData[i].plot(y="NumericValue",x="Year")
-#     else:
-#         regionData[i].plot(ax=ax,y="NumericValue",x="Year")
+topCountryData = []
+for i in range(numberOfTopCountries):
+    extractDataToArray(topCountries[i],"BTSX",topCountryData)
 
-# plt.title("Age-standardized suicide rates by Region")
-# plt.xlabel("Year")
-# plt.ylabel("Suicide rate per 100,000 population")
-# plt.xlim([2000,2019])
-# plt.legend(spatialRegions, bbox_to_anchor=(1, 1.), loc='best', prop={'size': 8}, ncol=4)
-# plt.show()
+for i in range(numberOfTopCountries):
+    if i == 0:
+        ax = topCountryData[i].plot(y="NumericValue",x="Year",)
+    else:
+        topCountryData[i].plot(ax=ax,y="NumericValue",x="Year")
+
+plt.title(f"Change in the {numberOfTopCountries} highest 2019 age-standardized suicide rates")
+plt.xlabel("Year")
+plt.ylabel("Suicide rate per 100,000 population")
+plt.xlim([2000,2019])
+plt.legend(topCountries,loc='best', prop={'size': 6}, ncol=4)
+plt.grid()
+plt.locator_params(axis="x", integer=True, tight=True)
+plt.show(block=False)
+
+#%% Lowest rates for N countries amongst all countries
+
+
+numberOfBottomCountries = 10
+bottomRates = []
+bottomCountries = []
+for n in range(numberOfBottomCountries):
+    bottomRates.append(sortedRecentRates[n])
+    bottomCountries.append(list(recentSuicideRates.keys())[list(recentSuicideRates.values()).index(sortedRecentRates[n])])
+
+bottomCountryData = []
+for i in range(numberOfBottomCountries):
+    extractDataToArray(bottomCountries[i],"BTSX",bottomCountryData)
+
+for i in range(numberOfBottomCountries):
+    if i == 0:
+        ax = bottomCountryData[i].plot(y="NumericValue",x="Year",)
+    else:
+        bottomCountryData[i].plot(ax=ax,y="NumericValue",x="Year")
+
+plt.title(f"Change in the {numberOfBottomCountries} lowest 2019 age-standardized suicide rates")
+plt.xlabel("Year")
+plt.ylabel("Suicide rate per 100,000 population")
+plt.xlim([2000,2019])
+plt.ylim([0,15])
+plt.legend(bottomCountries,loc='best', prop={'size': 6}, ncol=4)
+plt.grid()
+plt.locator_params(axis="x", integer=True, tight=True)
+plt.show()
